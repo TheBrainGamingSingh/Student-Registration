5e552819064f7f748647877b36e492eabe7cdb9e
TheBrainGamingSingh <manpreetsingh10399@gmail.com>
2020-07-17 22:16:47 +0530

    Made functions

--- send_mail.py
+++ send_mail.py
@@ -1,44 +1,40 @@
-import email, smtplib, ssl
+def send_email(receiver_email, filename):
+    import os
+    import email, smtplib, ssl
 
-from email import encoders
-from email.mime.base import MIMEBase
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+    from email import encoders
+    from email.mime.base import MIMEBase
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
 
-subject = "COVID - 19"
-body = "This is an email with attachment sent from your own COVID Tracker"
-body += f'\n\nTotal Cases in India as of {india.name} are \nConfirmed = {india[0]} \nActive Cases = {india[3]} \nDeaths = {india[1]} \nRecoveries = {india[2]} \n\n'
-india = i
-body += f'Your predictions as of {dt.datetime.now().strftime("%b %d, %Y")} are \nConfirmed = {india[0]} \nActive Cases = {india[3]} \nDeaths = {india[1]} \nRecoveries = {india[2]} \n\n'
+    subject = "Student Registration Slip"
+    body = "This is an automated email. For any queries contact: itsmanpreetsinghjuneja@gmail.com"
 
-sender_email = os.environ['EMAIL_USER']
-receiver_email = "itsmanpreetsinghjuneja@gmail.com"
-password = os.environ['EMAIL_PASS']
+    sender_email = os.environ['EMAIL_USER']
+    password = os.environ['EMAIL_PASS']
 
-message = MIMEMultipart()
-message["From"] = sender_email
-message["To"] = receiver_email
-message["Subject"] = subject
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = subject
 
-message.attach(MIMEText(body, "plain"))
+    message.attach(MIMEText(body, "plain"))
 
-filename = "results.xlsx"
+    with open(filename, "rb") as attachment:
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
 
-with open(filename, "rb") as attachment:
-    part = MIMEBase("application", "octet-stream")
-    part.set_payload(attachment.read())
+    encoders.encode_base64(part)
 
-encoders.encode_base64(part)
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {filename}",
+    )
 
-part.add_header(
-    "Content-Disposition",
-    f"attachment; filename= {filename}",
-)
+    message.attach(part)
+    text = message.as_string()
 
-message.attach(part)
-text = message.as_string()
-
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, text)
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, text)
--- word.py
+++ word.py
@@ -1,93 +1,90 @@
-from docx import Document
-from docx.enum.text import WD_ALIGN_PARAGRAPH
-from docx.enum.table import WD_TABLE_ALIGNMENT
-from docx.enum.table import WD_ALIGN_VERTICAL
+def df_to_word(student_id, student_name, open_ele_id, open_ele):
+	from docx import Document
+	from docx.enum.text import WD_ALIGN_PARAGRAPH
+	from docx.enum.table import WD_TABLE_ALIGNMENT
+	from docx.enum.table import WD_ALIGN_VERTICAL
 
-from docx.shared import Pt, Inches
+	from docx.shared import Pt, Inches
 
-document = Document()
+	document = Document()
 
-p = document.add_paragraph()
-p.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
-run = p.add_run('Punjab Engineering College, Chandigarh\n(Deemed To Be University)\n\n')
-run.bold = True
-font = run.font
-font.name = 'Times New Roman'
-font.size = Pt(24)
+	p = document.add_paragraph()
+	p.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+	run = p.add_run('Punjab Engineering College, Chandigarh\n(Deemed To Be University)\n\n')
+	run.bold = True
+	font = run.font
+	font.name = 'Times New Roman'
+	font.size = Pt(24)
 
-run = p.add_run('STUDENT REGISTRATION FORM\n\n')
-run.bold = True
-font = run.font
-font.name = 'Times New Roman'
-font.size = Pt(18)
+	run = p.add_run('STUDENT REGISTRATION FORM\n\n')
+	run.bold = True
+	font = run.font
+	font.name = 'Times New Roman'
+	font.size = Pt(18)
 
-p = document.add_paragraph()
-p.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.LEFT
+	p = document.add_paragraph()
+	p.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.LEFT
 
-student_id = '17103022'
-student_name = 'Manpreet Singh Juneja'
+	p.add_run('Student ID: ')
+	p.add_run('{}\t\t\t\t\t      '.format(student_id)).bold = True
 
-open_ele = ''
-dec1 = ''
-dec2 = ''
+	p.add_run('Name: ')
+	p.add_run('{}\n'.format(student_name)).bold = True
 
-p.add_run('Student ID: ')
-p.add_run('{}\t\t\t\t\t      '.format(student_id)).bold = True
-
-p.add_run('Name: ')
-p.add_run('{}\n'.format(student_name)).bold = True
-
-p.add_run('Department: CSE\t\t\t')
-p.add_run('Academic Session: 2020-21\t\t')
-p.add_run('Semester: 7th\n')
+	p.add_run('Department: CSE\t\t\t')
+	p.add_run('Academic Session: 2020-21\t\t')
+	p.add_run('Semester: 7th\n')
 
 
-records = (
-			(1, '1', 'A'),
-			(2, '2', 'B'),
-			(3, '3', 'C'),
-			(4, 'CSN441', 'Major Project')
-		  )
+	records = (
+				(1, open_ele_id, open_ele),
+				(2, dec1_id, dec1),
+				(3, dec2_id, dec2),
+				(4, 'CSN441', 'Major Project')
+			  )
 
-table = document.add_table(rows=1, cols=3, style='Table Grid')
+	table = document.add_table(rows=1, cols=3, style='Table Grid')
 
-headers = table.rows[0].cells
-headers[0].text = 'Sr. No.'
-headers[1].text = 'Course ID'
-headers[2].text = 'Course Name'
+	headers = table.rows[0].cells
+	headers[0].text = 'Sr. No.'
+	headers[1].text = 'Course ID'
+	headers[2].text = 'Course Name'
 
-for cell in headers:
-	cell.vertical_allignment = WD_ALIGN_VERTICAL.CENTER
+	for cell in headers:
+		cell.vertical_allignment = WD_ALIGN_VERTICAL.CENTER
 
-for sr_no, course_id, course_name in records:
-    tds = table.add_row().cells
-    tds[0].text = str(sr_no)
-    tds[1].text = course_id
-    tds[2].text = course_name
+	for sr_no, course_id, course_name in records:
+	    tds = table.add_row().cells
+	    tds[0].text = str(sr_no)
+	    tds[1].text = course_id
+	    tds[2].text = course_name
 
-for cell in table.columns[0].cells:
-    cell.width = Inches(0.4)
+	for cell in table.columns[0].cells:
+	    cell.width = Inches(0.4)
 
-for cell in table.columns[1].cells:
-    cell.width = Inches(3)
+	for cell in table.columns[1].cells:
+	    cell.width = Inches(3)
 
-for cell in table.columns[2].cells:
-    cell.width = Inches(3)
+	for cell in table.columns[2].cells:
+	    cell.width = Inches(3)
 
-p = document.add_paragraph()
-p.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
-p.add_run('\n\n')
+	p = document.add_paragraph()
+	p.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+	p.add_run('\n\n')
 
-table = document.add_table(rows=1, cols=2, style='Table Grid')
+	table = document.add_table(rows=1, cols=2, style='Table Grid')
 
-headers = table.rows[0].cells
-headers[0].text = 'Faculty Incharge'
-headers[1].text = 'Signature of Student'
+	headers = table.rows[0].cells
+	p = headers[0].add_paragraph('Faculty Incharge')
+	p.alignment = WD_ALIGN_PARAGRAPH.CENTER
+	p = headers[1].add_paragraph('Signature of Student')
+	p.alignment = WD_ALIGN_PARAGRAPH.CENTER
 
-for cell in table.rows[0].cells:
-	cell.alignment = WD_TABLE_ALIGNMENT.CENTER
-	cell.vertical_allignment = WD_ALIGN_VERTICAL.BOTTOM
-	cell.height = Inches(1.2)
+	for cell in table.rows[0].cells:
+		cell.vertical_allignment = WD_ALIGN_VERTICAL.BOTTOM
+
+	for row in table.rows:
+	    row.height = Inches(1.2)
 
 
-document.save('{}.docx'.format(student_id))
+	document.save('{}.docx'.format(student_id))
